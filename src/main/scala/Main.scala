6d3b191bebbf37acafb176ef3a3298353e015022
Ed <emaphis85@gmail.com>
2026-09-04 22:02:07 -0400

    Set up new Scala project

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+@main def hello(): Unit =
+  println("Hello world!")
+  println(msg)
+
+def msg: String = "I was compiled by Scala " + getVersion + " :)"
+
+def getVersion: String =
+  val compilerVersion = scala.util.Properties.versionString
+  compilerVersion
